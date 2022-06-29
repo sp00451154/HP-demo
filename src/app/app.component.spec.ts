78978d83e2def038150225b536de41cc7fcaa8cf
siba451154 <pandasibashankar@gmail.com>
2022-06-29 12:03:12 +0530

    all chanegs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-editable-table'`, () => {
+  it(`should have as title 'hp-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-editable-table');
+    expect(app.title).toEqual('hp-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-editable-table app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hp-demo app is running!');
   });
 });
